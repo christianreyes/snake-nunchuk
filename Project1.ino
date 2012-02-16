1d0119eb1b2c33959546c26366112da56bf1b2ca
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-02-15 23:02:19 -0500

    up button produces flash

--- Project1.ino
+++ Project1.ino
@@ -16,6 +16,9 @@ const int col[8] = { 10, 11, 12 , A3, A5, A4, A1, A2 };
 
 const int row[8] = {  2, 7,  4 , 3, 5 , 6, 8, 9 };
 
+const int upSwitch   = 1;
+const int downSwitch = 0;
+
 int pixels[8][8] = {
   { 0,0,0,0,0,0,0,0 },
   { 0,1,1,0,0,1,1,0 },
@@ -46,11 +49,28 @@ void loop() {
     for(int c=0;c<8;c++){
       digitalWrite(col[c], pixels[r][c] ? HIGH : LOW );
     }
-    delayMicroseconds(250);
+    //delayMicroseconds(650);
+    delay(1);
     for(int c=0;c<8;c++){
       digitalWrite(col[c], LOW );
     }
     digitalWrite( row[r], HIGH);
   }
+  
+  if( digitalRead(upSwitch) == HIGH){
+    flashAll();
+  }
 }  // end loop()
 
+void flashAll(){
+  for(int r=0;r<8;r++){
+    digitalWrite(row[r], LOW);
+    for(int c=0;c<8;c++){
+      digitalWrite(col[c], HIGH);
+    }
+    delay(100);
+    for(int c=0;c<8;c++){
+      digitalWrite(col[c], LOW);
+    }
+  }
+}
