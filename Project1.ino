22effa5b4fac31bb65ab85fe7052f58cf2eb06e9
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-02-17 04:03:50 -0500

    working with nunchuck

--- Project1.ino
+++ Project1.ino
@@ -6,11 +6,14 @@
 */
 /*========================================================*/
 
+#include <Wire.h>
+#include "nunchuck_funcs.h"
+
 /*------- Hardware configuration -------*/
 
 //                   1   2   3   4   5     6   7   8
 
-const int col[8] = { 10, 11, 12 , A3, 13, 0, A1, A2 };
+const int col[8] = { 10, 11, 12 , A3, 13, 13, A1, A2 };
 
 //                   1   2   3   4   5   6   7   8
 
@@ -19,21 +22,15 @@ const int row[8] = {  2, 7,  4 , 3, 5 , 6, 8, 9 };
 const int upSwitch   = 1;
 const int downSwitch = 0;
 
-int pixels[8][8] = {
-  { 0,0,0,0,0,0,0,0 },
-  { 0,1,1,0,0,1,1,0 },
-  { 0,1,1,0,0,1,1,0 },
-  { 0,0,0,0,0,0,0,0 },
-  { 0,1,1,0,0,1,1,0 },
-  { 0,1,1,1,1,1,1,0 },
-  { 0,0,1,1,1,1,0,0 },
-  { 0,0,0,0,0,0,0,0 } 
-};
+int x = 0;
+int y = 0;
+
+int timex = 0;
+int timey = 0;
 
 /*-------------------------------------------*/
 /* Initializization code (run once via call from Arduino framework) */
 void setup() {
-  Serial.begin(19200);
   
   // establish direction of pins we are using to drive LEDs 
   for(int i=0; i< 8 ; i++){
@@ -49,41 +46,83 @@ void setup() {
     digitalWrite( row[r], LOW);
     for(int c=0;c<8;c++){
       digitalWrite(col[c], HIGH );
-      delay(20);
+      delay(15);
       digitalWrite(col[c], LOW );
     }
     digitalWrite( row[r], HIGH);
   }
+  
+  int smilie[8][8] = {
+    { 0,0,0,0,0,0,0,0 },
+    { 0,1,1,0,0,1,1,0 },
+    { 0,1,1,0,0,1,1,0 },
+    { 0,0,0,0,0,0,0,0 },
+    { 0,1,1,0,0,1,1,0 },
+    { 0,1,1,1,1,1,1,0 },
+    { 0,0,1,1,1,1,0,0 },
+    { 0,0,0,0,0,0,0,0 } 
+  };
+  
+  for(int i=0;i<100;i++){
+    for(int r=0;r<8;r++){
+      digitalWrite( row[r], LOW);
+      for(int c=0;c<8;c++){
+        digitalWrite(col[c], smilie[r][c] ? HIGH : LOW );
+      }
+      //delayMicroseconds(650);
+      delay(1);
+      for(int c=0;c<8;c++){
+        digitalWrite(col[c], LOW );
+      }
+      digitalWrite( row[r], HIGH);
+    }
+  }
+  
+  Serial.begin(19200);
+  
+  nunchuck_init(); // send the initilization handshake
   
 }
 
 /* Main routine (called repeated by from the Arduino framework) */
 void loop() {
-  for(int r=0;r<8;r++){
+  for(int r=0; r<8; r++){
     digitalWrite( row[r], LOW);
-    for(int c=0;c<8;c++){
-      digitalWrite(col[c], pixels[r][c] ? HIGH : LOW );
-    }
-    //delayMicroseconds(650);
-    delay(1);
-    for(int c=0;c<8;c++){
-      digitalWrite(col[c], LOW );
+    for(int c=0; c<8; c++){
+      if(c==(x-1) && r==(y-1)){
+        digitalWrite( col[x-1], HIGH);
+        delay(10);
+        digitalWrite( col[x-1], LOW);
+      }
     }
     digitalWrite( row[r], HIGH);
   }
   
-  analogWrite(A5, 255);
-}  // end loop()
-
-void flashAll(){
-  for(int r=0;r<8;r++){
-    digitalWrite(row[r], LOW);
-    for(int c=0;c<8;c++){
-      digitalWrite(col[c], HIGH);
-    }
-    delay(100);
-    for(int c=0;c<8;c++){
-      digitalWrite(col[c], LOW);
-    }
+  nunchuck_get_data();
+  
+  timex++;
+  if(timex > 3){
+    if(x < 8 && nunchuck_joyx() > 180){
+      timex = 0;
+      x+= 1;
+    } else if(x > 0 && nunchuck_joyx() < 50){
+      timex = 0;
+      x= x-1;
+    } 
   }
-}
+  
+  
+  timey++;
+  if(timey > 3){
+    if(y > 0 && nunchuck_joyy() > 180){
+      timey = 0;
+      y= y-1;
+    } else if( y < 8 && nunchuck_joyy() < 50){
+      timey = 0;
+      y+= 1;
+    } 
+  }
+  
+  
+  nunchuck_print_data();
+}  // end loop()
