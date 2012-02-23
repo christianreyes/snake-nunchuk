d281e400a01e2dc7208bfcacca38d19ea9da838a
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-02-22 21:08:18 -0500

    getting ready to make body longer

--- Project1.ino
+++ Project1.ino
@@ -9,45 +9,136 @@
 #include <Wire.h>
 #include "nunchuck_funcs.h"
 #include "enemy.h"
+#include "body.h"
 
 //                    1   2   3    4   5  6   7   8
 const int col[8] = { 13,  1, 11 ,  7,  0, A0, 4, A3 };
-const int row[8] = {  6,  2,  8 ,  5, A1,  9, A2,  10 };
+const int row[8] = {  6,  2,  8 ,  5, A1,  9, A2, 10 };
 
-int x;
-int y;
+body head;
+body mybody[8];
 
 int timex = 0;
 int timey = 0;
 
 int timemove = 0;
 
-const int numEnemies = 3;
+int dim = 0;
+
+const int numEnemies = 4;
 enemy enemies[numEnemies];
 
 /*-------------------------------------------*/
 /* Initializization code (run once via call from Arduino framework) */
 
 void setup() {
-  // establish direction of pins we are using to drive LEDs 
-  pinSetup();
-  
-  // display a test pattern to check if LEDs are connected properly
-  testPattern();
-  
-  // smilie test pattern
-  smilieTest();
+  pinSetup();    // establish direction of pins we are using to drive LEDs 
+  testPattern(); // display a test pattern to check if LEDs are connected properly
+  smilieTest();  // smilie test pattern
 
   //Serial.begin(19200);
   randomSeed(analogRead(A7));
   nunchuck_init(); // send the initilization handshake
   
-  for(int i=0;i<numEnemies;i++){
-    enemies[i] = generateEnemy();
+  for(int i=0;i<numEnemies;i++){ enemies[i] = generateEnemy(); }
+  
+  head.x = random(1,9);
+  head.y = random(1,9);
+}
+
+/* Main routine (called repeated by from the Arduino framework) */
+void loop() {
+  updateDisplay();
+  moveEnemies();
+  processUserInput();
+  calculateEating();
+}  // end loop()
+
+void updateDisplay(){
+  for(int r=0; r<8; r++){
+    digitalWrite( row[r], LOW);
+    
+    if( head.y == (r+1) ){
+      digitalWrite( col[head.x-1], HIGH);
+    }
+    
+    if(dim == 1){
+      for(int i=0; i<numEnemies; i++){
+        enemy e = enemies[i];
+        
+        if(inThisRow(r, e)){
+          digitalWrite( col[(int)e.x - 1], HIGH);
+        }
+      }
+    }
+    
+    //delay(1);
+    delayMicroseconds(500);
+    
+    for(int c=0; c<8; c++){
+      digitalWrite( col[c], LOW);
+    }
+    
+    digitalWrite( row[r], HIGH);
+  }
+}
+
+void moveEnemies(){
+  if(dim == 1){
+    timemove++;
+    if(timemove > 15){
+      for(int i=0; i< numEnemies; i++){
+        enemy e = enemies[i];
+        
+        if(inBounds(e)){
+          e.x += e.magnitude * cos(e.angle);
+          e.y -= e.magnitude * sin(e.angle);
+        } else {
+          e = generateEnemy();
+        }
+        enemies[i] = e;
+      }
+      timemove = 0;
+    }
+  }
+    
+  if(dim == 4){
+    dim = 0;
+  } else {
+    dim++;
+  }
+}
+
+void processUserInput(){
+  nunchuck_get_data();
+  
+  timex++;
+  if(timex > 15){
+    if(head.x < 8 && nunchuck_joyx() > 180){
+      timex = 0;
+      head.x+= 1;
+    } else if(head.x > 1 && nunchuck_joyx() < 80){
+      timex = 0;
+      head.x-= 1;
+    } 
+  } else if(timex > 30000){
+    timex = 0;
   }
   
-  x = random(1,9);
-  y = random(1,9);
+  
+  timey++;
+  if(timey > 15){
+    if(head.y > 1 && nunchuck_joyy() > 180){
+      timey = 0;
+      head.y--;
+    } else if( head.y < 8 && nunchuck_joyy() < 80){
+      timey = 0;
+      head.y++;
+    } 
+  } else if(timey > 30000){
+    timey = 0;
+  }
+  
 }
 
 enemy generateEnemy(){
@@ -61,7 +152,7 @@ enemy generateEnemy(){
       e.y = random(1,9);
     }
     
-    e.magnitude = (float)random(1,10) / 7;
+    e.magnitude = (float)random(1,10) / 6;
 
     int tx; 
     int ty;
@@ -70,27 +161,11 @@ enemy generateEnemy(){
       tx = random(4,6);
       ty = random(4,6);
     } else {
-      tx = x;
-      tx = y;
+      tx = head.x;
+      tx = head.y;
     }
     
     e.angle = atan2((float)-(ty - e.y), (float)tx - e.x );
-    
-    //e.bright = (float) random(60,80) / 100.0;
-    /*
-      Serial.print("Enemy x: ");
-      Serial.print(e.x);
-      Serial.print("\ty: ");
-      Serial.print(e.y);
-      Serial.print("\ttx: ");
-      Serial.print(tx);
-      Serial.print("\tty: ");
-      Serial.print(ty);
-      Serial.print("\tmag: ");
-      Serial.print(e.magnitude);
-      Serial.print("\tangle: ");
-      Serial.println(e.angle);
-    */
     
     return e;
 }
@@ -102,84 +177,6 @@ boolean inThisRow(int r, enemy e){
 boolean inBounds(enemy e){
   return ((int)e.y >= 1 && (int)e.y <= 8) && ((int)e.x >= 1 && (int)e.x <= 8);
 }
-
-/* Main routine (called repeated by from the Arduino framework) */
-void loop() {
-  for(int r=0; r<8; r++){
-    digitalWrite( row[r], LOW);
-    
-    if( y == (r+1) ){
-      digitalWrite( col[x-1], HIGH);
-    }
-    
-    for(int i=0; i<numEnemies; i++){
-      enemy e = enemies[i];
-      
-      if(inThisRow(r, e)){
-        digitalWrite( col[(int)e.x - 1], HIGH);
-      }
-    }
-    
-    delay(1);
-    
-    for(int c=0; c<8; c++){
-      digitalWrite( col[c], LOW);
-    }
-    
-    digitalWrite( row[r], HIGH);
-  }
-  
-  timemove++;
-  if(timemove > 10){
-    for(int i=0; i< numEnemies; i++){
-      enemy e = enemies[i];
-      
-      if(inBounds(e)){
-        e.x += e.magnitude * cos(e.angle);
-        e.y -= e.magnitude * sin(e.angle);
-      } else {
-        e = generateEnemy();
-      }
-      
-      enemies[i] = e;
-
-
-    }
-    
-    timemove = 0;
-  }
-  
-  nunchuck_get_data();
-
-  timex++;
-  if(timex > 10){
-    if(x < 8 && nunchuck_joyx() > 180){
-      timex = 0;
-      x+= 1;
-    } else if(x > 1 && nunchuck_joyx() < 80){
-      timex = 0;
-      x= x-1;
-    } 
-  } else if(timex > 30000){
-    timex = 0;
-  }
-  
-  
-  timey++;
-  if(timey > 10){
-    if(y > 1 && nunchuck_joyy() > 180){
-      timey = 0;
-      y= y-1;
-    } else if( y < 8 && nunchuck_joyy() < 80){
-      timey = 0;
-      y+= 1;
-    } 
-  } else if(timey > 30000){
-    timey = 0;
-  }
- 
-}  // end loop()
-
 
 void pinSetup(){
   for(int i=0; i< 8 ; i++){
